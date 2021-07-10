4130a7d6ceaa83beb33a5900a0031ea53d5b4e6c
Jack Wetherell <jack.wetherell@gmail.com>
2021-07-10 02:02:56 +0100

    Cleaned up code

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include "spline.h"
 
 
-// for convenience
+// For convenience
 using nlohmann::json;
 using std::string;
 using std::vector;
@@ -19,15 +19,13 @@ using std::cout;
 using std::endl;
 
 
-int lane = 1;               // Starting lane
-double ref_vel = 0;         // Reference velocity
-
-
 // Define constants
-double TIME_STEP = 0.02;
-double MERGE_DISTANCE = 30.0;
-double CLOSE_DISTANCE = 25.0;
-int INITIAL_LANE = 1;
+const double TIME_STEP = 0.02;
+const double SPEED_LIMIT = 50.0;
+const double MERGE_DISTANCE = 30.0;
+const double CLOSE_DISTANCE = 25.0;
+const double REACTION = 0.5;
+const int INITIAL_LANE = 1;
 
 
 class Point
@@ -73,6 +71,7 @@ Car::Car(double velocity_x, double velocity_y, float d, double s, int prev_size)
   this->s = s;
   this->future_s = this->s + ((double)prev_size*TIME_STEP*this->speed);
 }
+
 
 bool Car::is_in_lane(int lane)
 {
@@ -121,14 +120,23 @@ class AutonomousCar
     double d;
     double yaw;
     double speed;
+    double target_vel;
     int lane;
     bool too_close;
     bool safe;
-    AutonomousCar(double x, double y, double s, double d, double yaw, double speed);
+    AutonomousCar();
+    void update(double x, double y, double s, double d, double yaw, double speed);
 };
 
 
-AutonomousCar::AutonomousCar(double x, double y, double s, double d, double yaw, double speed)
+AutonomousCar::AutonomousCar()
+{
+  this->lane = INITIAL_LANE;
+  this->target_vel = 0;
+}
+
+
+void AutonomousCar::update(double x, double y, double s, double d, double yaw, double speed)
 {
   this->position.x = x;
   this->position.y = y;
@@ -136,10 +144,13 @@ AutonomousCar::AutonomousCar(double x, double y, double s, double d, double yaw,
   this->d = d;
   this->yaw = yaw;
   this->speed = speed;
-  this->lane = INITIAL_LANE;
   this->too_close = false;
   this->safe = false;
 }
+
+
+// Create autonomous car object 
+AutonomousCar autonomous_car = AutonomousCar();
 
 
 int main()
@@ -199,7 +210,7 @@ int main()
         if (event == "telemetry")
         {
         
-          // Main car's localization Data
+          // Main car's localization data
           double car_x = j[1]["x"];
           double car_y = j[1]["y"];
           double car_s = j[1]["s"];
@@ -215,158 +226,133 @@ int main()
           double end_path_s = j[1]["end_path_s"];
           double end_path_d = j[1]["end_path_d"];
 
-          // Sensor Fusion Data, a list of all other cars on the same side of the road.
+          // Sensor Fusion data, a list of all other cars on the same side of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
           json msgJson;
 
           // Initialise trajectories to define a path made up of (x,y) points that the car will visit sequentially every .02 seconds.
           vector<double> next_x_vals;
           vector<double> next_y_vals;
-
-          // // TEMP: Straight ahead
-          // double dist_inc 0.5;
-          // for (int i = 0; i < 50; ++i)
-          // {
-          //   next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
-          //   next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
-          // }
-
-          // // TEMP: One line at a time
-          // double dist_inc = 0.5;
-          // cout << previous_path_x.size() << endl;
-          // if (previous_path_x.size() > 0)
-          // {
-          //   for (int i = 0; i < previous_path_x.size(); ++i)
-          //   {
-          //     cout << previous_path_x[i] << " "  << previous_path_y[i] << endl;
-          //     next_x_vals.push_back(previous_path_x[i]);
-          //     next_y_vals.push_back(previous_path_y[i]);
-          //   }
-          // }
-          // else
-          // {
-          //   for (int i = 0; i < 50; ++i)
-          //   {
-          //     next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
-          //     next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
-          //   }
-          // }
-
-          // // TEMP: Stay in lane
-          // double dist_inc = 0.5;
-          // for (int i = 0; i < 50; ++i)
-          // {
-          //   double next_s = car_s+dist_inc*(i+1);
-          //   double next_d = car_d;
-          //   vector<double> xy = getXY(next_s, car_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-          //   next_x_vals.push_back(xy[0]);
-          //   next_y_vals.push_back(xy[1]);
-          // }
-
-
+          
+          // Determine how many points are remaining in the path from the last calculation
           int prev_size = previous_path_x.size();
 
+          // Place car at end of last path
           if (prev_size > 0)
           {
             car_s = end_path_s;
           }
 
-          bool too_close = false;
+          // Update autonomous car object
+          autonomous_car.update(car_x, car_y, car_s, car_d, car_yaw, car_speed);
 
+          // Loop over all cars in scene
           for (int i = 0; i < sensor_fusion.size(); i++)
           {
-            float d = sensor_fusion[i][6];
+            // Create car object
+            Car car = Car(sensor_fusion[i][3], sensor_fusion[i][4], sensor_fusion[i][6], sensor_fusion[i][5], prev_size);
 
             // Is the car in my lane?
-            if(d < (2+4*lane+2) && d > (2+4*lane-2))
+            if(car.is_in_lane(autonomous_car.lane))
             {
-              double vx = sensor_fusion[i][3];
-              double vy = sensor_fusion[i][4];
-              double check_speed = sqrt(pow(vx, 2.0) + pow(vy, 2.0));
-              double check_car_s = sensor_fusion[i][5];
-              double fut_check_car_s = check_car_s + ((double)prev_size*0.02*check_speed);
 
-              if((fut_check_car_s > car_s) && ((fut_check_car_s - car_s < 30)))
+              // Is the car too close to me?
+              if(car.is_too_close(autonomous_car.s))
               {
-                too_close = true;
-                bool safe = false;
+                autonomous_car.too_close = true;
 
-                // Try to change to the left
-                if (safe == false and lane > 0)
+                // Try to change to the left lane to overtake
+                if (autonomous_car.safe == false and autonomous_car.lane > 0)
                 {
-                  safe = true;
-                  int target_lane = lane - 1;
+                  autonomous_car.safe = true;
+                  int target_lane = autonomous_car.lane - 1;
                   for (int j = 0; j < sensor_fusion.size(); j++)
                   {
-                    double other_s = sensor_fusion[j][5];
-                    double other_d = sensor_fusion[j][6];
-                    if(other_d < (2+4*target_lane+2) && other_d > (2+4*target_lane-2))
+                    // Create merge car object
+                    Car merge_car = Car(sensor_fusion[j][3], sensor_fusion[j][4], sensor_fusion[j][6], sensor_fusion[j][5], prev_size);
+
+                    // Is the merge car in the target lane?
+                    if(merge_car.is_in_lane(target_lane))
                     {
-                      if((other_s > car_s - 30) && ((other_s < car_s + 20)))
+
+                      // Is it safe to merge to the lane with this car here?
+                      if(merge_car.can_be_merged(autonomous_car.s) == false)
                       { 
-                        safe = false;
+                        autonomous_car.safe = false;
                       }
                     }
                   }
-                  if (safe == true)
+
+                  // If it is safe change the terget lane
+                  if (autonomous_car.safe == true)
                   {
-                    lane = target_lane;
+                    autonomous_car.lane = target_lane;
                   }
                 }
 
-                if (safe == false and lane < 2)
+                // Try to change to the right lane to overtake
+                if (autonomous_car.safe == false and autonomous_car.lane < 2)
                 {
-                  safe = true;
-                  int target_lane = lane + 1;
+                  autonomous_car.safe = true;
+                  int target_lane = autonomous_car.lane + 1;
                   for (int j = 0; j < sensor_fusion.size(); j++)
                   {
-                    double other_s = sensor_fusion[j][5];
-                    double other_d = sensor_fusion[j][6];
-                    if(other_d < (2+4*target_lane+2) && other_d > (2+4*target_lane-2))
+
+                    // Create merge car object
+                    Car merge_car = Car(sensor_fusion[j][3], sensor_fusion[j][4], sensor_fusion[j][6], sensor_fusion[j][5], prev_size);
+
+                    // Is the merge car in the target lane?
+                    if(merge_car.is_in_lane(target_lane))
                     {
-                      if((other_s > car_s - 30) && ((other_s < car_s + 20)))
+
+                      // Is it safe to merge to the lane with this car here?
+                      if(merge_car.can_be_merged(autonomous_car.s) == false)
                       { 
-                        safe = false;
+                        autonomous_car.safe = false;
                       }
                     }
                   }
-                  if (safe == true)
+
+                  // If it is safe change the terget lane
+                  if (autonomous_car.safe == true)
                   {
-                    lane = target_lane;
+                    autonomous_car.lane = target_lane;
                   }
                 }
-
-
-
               }
             }
           }
 
-          if (too_close)
+          // If too close to car enfront -> slow down
+          if (autonomous_car.too_close)
           {
-            ref_vel -= 0.500; // 0.224;
-          }
-          else if (ref_vel < 49.5)
-          {
-            ref_vel += 0.500; // 0.224;
+            autonomous_car.target_vel -= REACTION;
           }
 
+          // If not -> reach just under speed limit
+          else if (autonomous_car.target_vel < SPEED_LIMIT - 0.5)
+          {
+            autonomous_car.target_vel += REACTION;
+          }
+
+          // Create vector of points to act as waypoints to build splines
           vector<double> pstx;
           vector<double> psty;
 
-          double ref_x = car_x;
-          double ref_y = car_y;
-          double ref_yaw = deg2rad(car_yaw);
+          // Get the ref positions
+          double ref_x = autonomous_car.position.x;
+          double ref_y = autonomous_car.position.y;
+          double ref_yaw = deg2rad(autonomous_car.yaw);
 
+          // Add two waypoints of the currrent and last car position to ensure smoothness
           if (prev_size < 2)
           {
-            double prev_car_x = car_x - cos(car_yaw);
-            double prev_car_y = car_y - sin(car_yaw);
-
+            double prev_car_x = autonomous_car.position.x - cos(autonomous_car.yaw);
+            double prev_car_y = autonomous_car.position.y - sin(autonomous_car.yaw);
             pstx.push_back(prev_car_x);
-            pstx.push_back(car_x);
+            pstx.push_back(autonomous_car.position.x);
             psty.push_back(prev_car_y);
-            psty.push_back(car_y);
+            psty.push_back(autonomous_car.position.y);
           }
           else
           {
@@ -375,17 +361,16 @@ int main()
             double ref_x_prev = previous_path_x[prev_size - 2];
             double ref_y_prev = previous_path_y[prev_size - 2];
             ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
-
             pstx.push_back(ref_x_prev);
             pstx.push_back(ref_x);
             psty.push_back(ref_y_prev);
             psty.push_back(ref_y);
           }
 
-          vector<double> wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
-          vector<double> wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
-          vector<double> wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
-
+          // Add three waypoints in the distance
+          vector<double> wp0 = getXY(autonomous_car.s+30, (2+4*autonomous_car.lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> wp1 = getXY(autonomous_car.s+60, (2+4*autonomous_car.lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> wp2 = getXY(autonomous_car.s+90, (2+4*autonomous_car.lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
           pstx.push_back(wp0[0]);
           pstx.push_back(wp1[0]);
           pstx.push_back(wp2[0]);
@@ -393,50 +378,54 @@ int main()
           psty.push_back(wp1[1]);
           psty.push_back(wp2[1]);
 
+          // Transform waypoints into AVs reference frame
           for (int i = 0; i < pstx.size(); i++)
           {
             double shift_x = pstx[i] - ref_x;
             double shift_y = psty[i] - ref_y;
-
             pstx[i] = (shift_x * cos(0-ref_yaw) - shift_y * sin(0-ref_yaw));
             psty[i] = (shift_x * sin(0-ref_yaw) + shift_y * cos(0-ref_yaw));
           }
           
+          // Add waypoints to spline
           tk::spline s;
           s.set_points(pstx, psty);
 
+          // Start with the previous path
           for (int i = 0; i < prev_size; i++)
           {
             next_x_vals.push_back(previous_path_x[i]);
             next_y_vals.push_back(previous_path_y[i]);
           }
 
+          // Define the target x,y position
           double target_x = 30.0;
           double target_y = s(target_x);
           double target_dist = sqrt(pow(target_x, 2.0) + pow(target_y, 2.0));
           double x_add_on = 0;
 
+          // Add new points to the path
           for (int i = 1; i <= 50 - prev_size; i++)
           {
-            double N = (target_dist/(0.02*ref_vel/2.24));
+
+            // Split spine into N points to ensure correct speed
+            double N = (target_dist/(0.02*autonomous_car.target_vel/2.24));
             double x_point = x_add_on + target_x / N;
             double y_point = s(x_point);
-
             x_add_on = x_point;
-
             double x_ref = x_point;
             double y_ref = y_point;
 
+            // Transform the points back to the world reference frame
             x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
             y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
-
             x_point += ref_x;
             y_point += ref_y;
 
+            // Add the new points to the vector
             next_x_vals.push_back(x_point);
             next_y_vals.push_back(y_point);
           }
-
 
           // Websocket communitcation
           msgJson["next_x"] = next_x_vals;
